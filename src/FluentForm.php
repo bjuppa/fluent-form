a8f769be7cae5b1252b5adfb2cb5edd87fcd3a2a
bjuppa <nilsved@gmail.com>
2016-01-10 14:06:49 +0100

    base for FluentForm and FormBlockContainer

---
+++ src/FluentForm.php
@@ -0,0 +1,17 @@
+<?php
+namespace FewAgency\FluentForm;
+
+use FewAgency\FluentForm\FormBlockContainer\FormBlockContainer;
+
+class FluentForm extends FormBlockContainer
+{
+
+    /*
+TODO: make default attribute method=POST
+
+TODO: implement these methods on FluentForm
+->withToken(token, name=_token)
+->withMethod(method, name=_method) sets hidden REST method if needed
+->withAction(url)
+    */
+}
---
+++ src/FormBlockContainer/FormBlockContainer.php
@@ -0,0 +1,29 @@
+<?php
+namespace FewAgency\FluentForm\FormBlockContainer;
+
+use FewAgency\FluentHtml\FluentHtml;
+
+abstract class FormBlockContainer extends FluentHtml
+{
+    protected $is_aligned;
+    protected $alignment_classes;
+    protected $alignment_classes_default = [1 => 'half-width float-left align-right', 2 => 'half-width', 3 => ''];
+    protected $alignment_offset_classes;
+    protected $alignment_offset_classes_default = [2 => 'half-width half-margin-left', 3 => ''];
+
+    /* TODO: implement these methods on FormBlockContainer
+->getForm()
+->withAlignmentClasses(col 1, col 2, col 3, offset 2, offset 3=null)
+->getAlignmentClasses(column number, bool with_offset=false)
+->align(true)
+->isAligned()
+->withValues(object or array)
+->withValues(overriding values)
+->withErrors(messages)
+->withWarnings(messages)
+->withHiddenInput(name, value)
+->with…Block(name, type)
+->with…Blocks(array of input names and types)
+->containing…Block(name)->withLabel(text)->followedBy…Block()
+     */
+}
