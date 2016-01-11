004a4a48c412850fd010a44e31de1d2b43244142
bjuppa <nilsved@gmail.com>
2016-01-11 16:55:06 +0100

    base FormBlock

---
+++ src/FormBlock/FormBlock.php
@@ -0,0 +1,30 @@
+<?php
+namespace FewAgency\FluentHtml\FormBlock;
+
+use FewAgency\FluentHtml\FluentHtml;
+
+abstract class FormBlock extends FluentHtml
+{
+    /* TODO: implement these methods on FormBlock:
+    ->withLabel(text)
+    ->getForm()
+    ->getFormBlockContainer()
+    ->getAlignmentClasses(column number, bool with_offset=false)
+    ->followedBy…Block()
+    ->disabled(true)
+    ->readonly(true)
+    ->required(true)
+    ->withDescription(text)
+    ->withError(messages)
+    ->withWarning(messages)
+    ->withSuccess(true)
+    ->withFeedback(html)
+    ->getInputElement()
+    ->getLabelElement()
+    ->getDescriptionElement()
+    ->getColumnElement(column number)
+    ->getScreenReaderOnlyClass()
+    ->hideLabel()
+    ->withInputAttribute(attributes)
+     */
+}
