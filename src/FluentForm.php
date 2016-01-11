e1454ec1cfd4ce523970662cbdc08f97ac4ca222
bjuppa <nilsved@gmail.com>
2016-01-11 13:34:30 +0100

    check for method

--- src/FluentForm.php
+++ src/FluentForm.php
@@ -13,18 +13,38 @@ class FluentForm extends FormBlockContainer
         $tag_contents = null;
         $tag_attributes = [
             'method' => function (FluentForm $form) {
-                return $form->form_method == 'GET' ? $form->form_method : 'POST';
+                return $form->hasMethodGet() ? $form->form_method : 'POST';
             }
         ];
         parent::__construct($html_element_name, $tag_contents, $tag_attributes);
     }
 
     /**
-     * @param string|callable $url to set as form's action
+     * Set action url on form
+     * @param string|callable|false $url to set as form's action
      */
     public function withAction($url)
     {
         $this->withAttribute('action', $url);
+    }
+
+    /**
+     * Check if form has a specified method set
+     * @param string $method to check for
+     * @return bool
+     */
+    public function hasMethod($method)
+    {
+        return strtoupper($this->form_method) == strtoupper($method);
+    }
+
+    /**
+     * Check if method is GET
+     * @return bool true if form's method is GET
+     */
+    public function hasMethodGet()
+    {
+        return $this->hasMethod('GET');
     }
 
     /*
