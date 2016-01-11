5b0f1996a280e260307de2858ef7f5e3b362df6a
bjuppa <nilsved@gmail.com>
2016-01-11 15:54:36 +0100

    Select stub

---
+++ src/FormInput/Select.php
@@ -0,0 +1,12 @@
+<?php
+namespace FewAgency\FluentForm\FormInput;
+
+
+class Select
+{
+    //TODO: use traits for common input functionality for Select
+    /* TODO: implement methods on Select:
+->withOptions()
+->multiple(true) appends [] to name attribute if true
+    */
+}
