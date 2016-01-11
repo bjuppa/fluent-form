d6ea9703879e6f3e75dd52f9c5d317152492966b
bjuppa <nilsved@gmail.com>
2016-01-11 15:47:10 +0100

    completed todo

--- src/FluentForm.php
+++ src/FluentForm.php
@@ -67,7 +67,6 @@ class FluentForm extends FormBlockContainer
 
     /*
 TODO: implement these methods on FluentForm
-->withToken(token, name=_token)
 ->withMethod(method, name=_method) sets hidden REST method if needed
     */
 }
