146ade0bd13324bab2517dcd3c011520be94422a
bjuppa <nilsved@gmail.com>
2016-01-11 13:34:36 +0100

    test constructor

--- tests/FluentFormTest.php
+++ tests/FluentFormTest.php
@@ -1,8 +1,15 @@
 <?php
 
 use FewAgency\FluentHtml\Testing\ComparesFluentHtml;
+use FewAgency\FluentForm\FluentForm;
 
 class FluentFormTest extends PHPUnit_Framework_TestCase
 {
     use ComparesFluentHtml;
+
+    public function testConstructor()
+    {
+        $f = new FluentForm();
+        $this->assertHtmlEquals('<form method="POST"></form>', $f);
+    }
 }
