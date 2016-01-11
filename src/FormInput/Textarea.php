810de29e7c6228bde28e7274b48f986222fe38b6
bjuppa <nilsved@gmail.com>
2016-01-11 15:50:23 +0100

    Textarea stub

---
+++ src/FormInput/Textarea.php
@@ -0,0 +1,9 @@
+<?php
+namespace FewAgency\FluentForm\FormInput;
+
+
+class Textarea
+{
+    //TODO: use traits for common input functionality for Textarea
+    //TODO: make vithValue($value) set element content on Textarea
+}
