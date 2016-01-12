5bce9610a96c617f6560957c71e1184b62aa8c14
bjuppa <nilsved@gmail.com>
2016-01-12 13:24:38 +0100

    handling classnames as input type

--- src/FormBlock/InputBlock.php
+++ src/FormBlock/InputBlock.php
@@ -7,14 +7,16 @@ class InputBlock extends FormBlock
 {
     /**
      * InputBlock constructor.
-     * @param string $name
-     * @param string $type
+     * @param string $name of input
+     * @param string $type of input or fully qualified classname
      */
     public function __construct($name, $type = 'text')
     {
         $classname = '\FewAgency\FluentForm\FormInput\typeInputElement' . $type . 'InputElement';
         if (class_exists($classname)) {
             $input = new $classname($name);
+        } elseif (class_exists($type)) {
+            $input = new $type($name);
         } else {
             $input = new TextInputElement($name, $type);
         }
