45c3fa908c11783347ca5d69113fc59ee9b51f5e
bjuppa <nilsved@gmail.com>
2016-01-12 10:39:30 +0100

    docs

--- src/Support/FormElementContract.php
+++ src/Support/FormElementContract.php
@@ -1,9 +1,13 @@
 <?php
 namespace FewAgency\FluentForm\Support;
 
+/*
+ * Contract for all FluentHtml elements that are in forms
+*/
 interface FormElementContract
 {
     /**
+     * Get the form element for the element
      * @return \FewAgency\FluentForm\FluentForm
      */
     public function getForm();
