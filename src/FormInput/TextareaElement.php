3e2d3bc3b013eaa4f36c2ad83ea4e856dd073815
bjuppa <nilsved@gmail.com>
2016-02-22 11:31:12 +0100

    todo

--- src/FormInput/TextareaElement.php
+++ src/FormInput/TextareaElement.php
@@ -4,6 +4,6 @@ namespace FewAgency\FluentForm\FormInput;
 
 class TextareaElement extends FormInputElement
 {
-    //TODO: use traits for common input functionality for TextareaElement
+    //TODO: use traits for common input functionality for TextareaElement (e.g. shared with InputElement)
     //TODO: constructor should set default content to the getValue() for the input name
 }
